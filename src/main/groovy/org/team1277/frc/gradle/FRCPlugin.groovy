e10464dea8add9bfa92888b8027e08028239be16
Ben Navetta <ben.navetta@gmail.com>
2014-11-05 19:24:07 -0500

    stub code

---
+++ src/main/groovy/org/team1277/frc/gradle/FRCPlugin.groovy
@@ -0,0 +1,12 @@
+package org.team1277.frc.gradle;
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class FRCPlugin implements Plugin<Project>
+{
+	void apply(Project project)
+	{
+	}
+}
+
