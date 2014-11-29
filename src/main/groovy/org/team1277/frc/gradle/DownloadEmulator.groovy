dbf6b43b21d3318e1c9c5fd975ce57ce27a7340f
Ben Navetta <ben.navetta@gmail.com>
2014-11-29 16:32:16 -0500

    update example build.gradle with new version and name

---
+++ src/main/groovy/org/team1277/frc/gradle/DownloadEmulator.groovy
@@ -0,0 +1,11 @@
+package org.team1277.frc.gradle
+
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+
+/**
+ * FRC emulator setup.
+ */
+class DownloadEmulator
+{
+	static final String EMULATOR_LOCATION = 'https://dl.dropboxusercontent.com/u/40572037/frcEmulator.jar'
+}
